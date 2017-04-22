28aac15457a792dbead29f246bc32df52b1a56e8
Song Li <lisong1013@gmail.com>
2017-04-22 16:42:13 -0400

    new

--- result/js/get_result.js
+++ result/js/get_result.js
@@ -30,15 +30,14 @@ function get_result() {
   var keys = get_keys();
   console.log(keys);
   for (var idx in keys) {
-    parts = keys[idx].split('~')
+    parts = keys[idx].split('_')
     ip = parts[0];
     time = parts[1];
     browser = parts[2];
-    os = parts[3];
-    id = parts[4];
+    id = parts[3];
 
-    var b_1 = $('<option value = "' + id + '">' + ip + '_' + os + '_' + browser + '_' + time + '</option>');
-    var b_2 = $('<option value = "' + id + '">' + ip + '_' + os + '_' + browser + '_' + time + '</option>');
+    var b_1 = $('<option value = "' + id + '">' + ip + '_' + browser + '_' + time + '</option>');
+    var b_2 = $('<option value = "' + id + '">' + ip + '_' + browser + '_' + time + '</option>');
     $("#select_1").append(b_1);
     $("#select_2").append(b_2);
   }
